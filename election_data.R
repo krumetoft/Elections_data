9c98918ef6edc7cda3d388f1af1e6ed187fbb783
krumetoft <krum.arnaudov@ft.com>
2021-08-05 10:10:53 +0300

    Added a utils file and updated the script

--- election_data.R
+++ election_data.R
@@ -1,10 +1,12 @@
 library(tidyverse)
-library(ggbiplot)
-library(plotly)
+source("utils.R")
+#library(ggbiplot)
+#library(plotly)
 #library(devtools)
 #install_github("vqv/ggbiplot")
 
 #Making sure the output in console is readable
+#Seems to only be needed in Windows, worked well on a MAC
 #Sys.setlocale("LC_CTYPE", "bulgarian")
 
 #Links to files
@@ -12,76 +14,13 @@ parties_link <-'https://raw.githubusercontent.com/krumeto/Elections_data/main/el
 sections_link <- 'https://raw.githubusercontent.com/krumeto/Elections_data/main/elections/elections_2021_july/sections_11.07.2021.txt'
 votes_link <- "https://raw.githubusercontent.com/krumeto/Elections_data/main/elections/elections_2021_july/votes_11.07.2021.txt"
 
-#functions
-read_parties_table <- function(link_to_file){
-  read.delim(link_to_file, 
-             sep=";", 
-             encoding = "UTF-8",
-             header = FALSE,
-             col.names = c('party_index', 'party_name')
-             )
-}
 
-read_sections_table <- function(link_to_file){
-  read.delim(link_to_file, 
-             sep=";", 
-             encoding = "UTF-8",
-             header = FALSE,
-             col.names = c('section_code', 'administrative_entity_code', 'administrative_entity_name', 'ekatte_code', 'location', 'mobile_section?', 'ship_section?', 'machine_number')
-  )
-}
-
-read_prep_votes_table <- function(link_to_file, party_df){
-  votes <- read.delim(link_to_file, 
-             sep=";", 
-             encoding = "UTF-8",
-             header = FALSE
-  )
-  names(votes)[1] <- 'form_number'
-  names(votes)[2] <- 'section_code'
-  names(votes)[3] <- 'identificator_administrative_entity'
-  
-  
-  names(votes)[grepl("V", names(votes)) & parse_number(names(votes)) %% 2 == 0] <- party_df$party_name
-  names(votes)[grepl("V", names(votes)) & parse_number(names(votes)) %% 2 == 1] <- paste(party_df$party_name, "_votes", sep='')
-  
-  votes
-}
-
-extract_votes_per_party <- function(votes_df, party_name){
-  
-  votes_df %>% 
-    select(starts_with(c('form_number', 'section_code', 'identificator_administrative_entity'))|starts_with(party_name)) %>% 
-    rename(party_index := !!party_name) %>% 
-    rename(votes_count := !!paste(party_name, "_votes", sep='')) 
-  
-}
-
-finalize_votes_df <- function(votes_link, parties_df){
-  
-  raw_df <- read_prep_votes_table(votes_link, parties_df)
-  
-  datalist <- list()
-  
-  for (party_ix in seq_along(parties_df$party_name)) {
-    temp_df <- extract_votes_per_party(raw_df, parties_df$party_name[party_ix])
-    datalist[[party_ix]] <- temp_df
-  }
-  
-  full_votes_df <- bind_rows(datalist)
-  full_votes_df
-}
-
-finalize_votes_df(votes_link, parties)
-
-
+#Read Sections, Parties and Votes
 sections <- read_sections_table(sections_link)
-
 parties <- read_parties_table(parties_link)
-
 votes <- finalize_votes_df(votes_link, parties)
 
-
+--------------------------------
 votes %>% 
   group_by(party_index) %>% 
   summarize(sum_votes = sum(votes_count, na.rm = TRUE)) %>% 
