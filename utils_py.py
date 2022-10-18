653703ae1cc50c1294091a713a9fc6c7144a882f
krumetoft <krum.arnaudov@ft.com>
2022-10-18 10:45:35 +0300

    some outlier work

--- utils_py.py
+++ utils_py.py
@@ -45,9 +45,8 @@ def read_sections(link_to_file):
     engine = 'python'
     )
 
-def read_votes(link_to_file):
 
-    results_df = pd.DataFrame()
+def read_votes(link_to_file):
 
     with open(link_to_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
@@ -59,78 +58,48 @@ def read_votes(link_to_file):
 
             parties_iter = iter(row[3:])
             parties_dict = dict(zip(parties_iter, parties_iter))
-            results_list.append([n_form, section_code, admin_entity_code, parties_dict])
 
-            section_df = pd.DataFrame(parties_dict, index = [0]).T.reset_index()
-            section_df.columns = ['party_code', 'votes']
-            section_df = section_df.assign(
-                n_form = n_form,
-                section_code = section_code,
-                admin_entity_code = admin_entity_code
-            )
+            section_results_list = [(n_form, section_code, admin_entity_code, k, v) for k,v in parties_dict.items()]
+            results_list = [*results_list,*section_results_list]
 
-            results_df = pd.concat([results_df, section_df])
-
-    return results_df
-
-initial_fields = ['form_nr', 'section_code', 'admin_entity_code'] 
-parties_codes = list(range(1,31))
-parties_votes = [str(x) + '_votes' for x in parties_codes]
-zipped_thing = list(zip(parties_codes, parties_votes))
-list(chain.from_iterable(zipped_thing))
-
-len(initial_fields + parties_codes)
-    Полета:
-  1) № формуляр
-  2) Пълен код на секция(код на район(2), община(2), адм. район(2), секция(3));
-  3) Идентификатор на административна единица, за която се отнася протокола(община, кметство, район)
-Следват гласовете за всяка партия, коалиция, инициативен комитет, според съответната номенклатура (в ЦИК или РИК), като данните са в последователност № П/К/ИК;действителни гласове
+    return pd.DataFrame(results_list, columns=['nr_form', 'section_code', 'admin_entity_code', 'party_code', 'votes']).astype({'votes': 'int32'})
 
 
 read_parties(PARTIES_LINK).pipe(add_party_alias)
 read_sections(SECTIONS_LINK).head()
 
-len(read_votes(VOTES_LINK).columns)
-read_votes(VOTES_LINK)
-
-test_s = "32;022300017;2;1;77;2;1;3;0;4;1;5;1;6;1;7;2;8;0;9;1;10;0;11;0;12;0;13;0;14;0;15;2;16;0;17;0;18;0;19;0;20;0;22;0;23;1;24;42;25;1;26;0;27;0;28;2;29;0"
-test_s.count(";")
-
-pd.DataFrame(read_votes(VOTES_LINK).votes_dict[0], index = [0]).T.reset_index()
-
-import csv
-
-with open(VOTES_LINK) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
-    results_list = []
-    for row in csv_reader:
-        while line_count < 5:
-            n_form = row[0]
-            section_code = row[1]
-            admin_entity_code = row[2]
-
-            parties_iter = iter(row[3:])
-            parties_dict = dict(zip(parties_iter, parties_iter))
-            results_list.append([n_form, section_code, admin_entity_code, parties_dict])
-            line_count += 1
+votes_per_section = read_votes(VOTES_LINK)
+X = votes_per_section.groupby(['section_code', 'party_code']).agg({'votes': 'sum'}).unstack('party_code').fillna(0)
 
 
-def read_votes(link_to_file):
-    with open(link_to_file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=';')
-        results_list = []
-        for row in csv_reader:
-            n_form = row[0]
-            section_code = row[1]
-            admin_entity_code = row[2]
-            parties_iter = iter(row[3:])
-            parties_dict = dict(zip(parties_iter, parties_iter))
+def votes_section_wider(votes_df):
+    return votes_df.groupby(['section_code', 'party_code']).agg({'votes': 'sum'}).unstack('party_code').fillna(0)
 
-            section_results_list = [(n_form, section_code, admin_entity_code, k, v) for k,v in parties_dict.items()]
 
-            results_list = [*results_list,*section_results_list]
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import IsolationForest
 
-    return pd.DataFrame(results_list, columns=['nr_form', 'section_code', 'admin_entity_code', 'party_code', 'votes']).astype({'votes': 'int32'})
+clf = make_pipeline(RobustScaler(), IsolationForest(max_samples=100, random_state=2022))
+clf.fit(X)
 
-interim = read_votes(VOTES_LINK)
+y_pred_train = clf.predict(X)
+
+X.assign(outliers = y_pred_train).loc[lambda d: d['outliers'] == -1]
+
+
+import umap
+import matplotlib.pyplot as plt
+reducer = umap.UMAP()
+X_reduced = make_pipeline(RobustScaler(), reducer).fit_transform(X)
+
+
+plt.scatter(
+    X_reduced[:, 0],
+    X_reduced[:, 1],
+    c=y_pred_train,
+    alpha=0.3
+    )
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('Outlier sections', fontsize=24);
+plt.show()
